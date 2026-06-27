1d6bc7a7ba17c0398050facd80501d6b00e09e44
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2026-06-28 05:31:01 +0800

    style: format variable definitions for consistency in variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,29 @@
 variable "project_id" {
-	description = "The GCP Project ID where IAM bindings will be applied."
-	type        = string
+  description = "The GCP Project ID where IAM bindings will be applied."
+  type        = string
 }
 
 variable "region" {
-	description = "The GCP region for any regional resources (if needed)."
-	type        = string
-	default     = "us-central1"
+  description = "The GCP region for any regional resources (if needed)."
+  type        = string
+  default     = "us-central1"
 }
 
 variable "team_permissions" {
-	description = "Map of free consumer Google Groups to their assigned GCP roles."
-	type        = map(list(string))
-	default = {
-		"my-free-devops-team@googlegroups.com" = [
-			"roles/compute.admin",
-			"roles/run.admin",
-			"roles/cloudbuild.admin",
-			"roles/storage.admin"
-		],
-		"my-free-dev-team@googlegroups.com" = [
-			"roles/compute.viewer",
-			"roles/run.developer",
-			"roles/cloudbuild.editor",
-			"roles/storage.objectUser"
-		]
-	}
+  description = "Map of free consumer Google Groups to their assigned GCP roles."
+  type        = map(list(string))
+  default = {
+    "my-free-devops-team@googlegroups.com" = [
+      "roles/compute.admin",
+      "roles/run.admin",
+      "roles/cloudbuild.admin",
+      "roles/storage.admin"
+    ],
+    "my-free-dev-team@googlegroups.com" = [
+      "roles/compute.viewer",
+      "roles/run.developer",
+      "roles/cloudbuild.editor",
+      "roles/storage.objectUser"
+    ]
+  }
 }
