f31a280f084d471b967f37f256bc0839a42862d1
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2026-07-01 00:01:26 +0800

    refactor: remove obsolete Terraform settings and remote state locking from main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-# ==========================================
-# 1. TERRAFORM SETTINGS & REMOTE STATE LOCKING
-# ==========================================
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 7.0"
-    }
-  }
-
-  backend "gcs" {
-  }
-}
-
 provider "google" {
   project = var.project_id
   region  = var.region
